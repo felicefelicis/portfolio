fd7ed009aed1620e3243a8e5ac06dbbb8a2d1503
Kathleen Blackburn <inwie.l@gmail.com>
2017-03-19 13:57:48 -0400

    correction du background dacceuil, ajout du menu et des liens actifs.  Manque toujours le lightbox

--- js/skill.js
+++ js/skill.js
@@ -1,9 +1,7 @@
 /* fonction background aléatoire*/
 (function() {
         var backgrounds = [
-			"images/bg1.jpeg",
-			"images/bg2.jpeg",
-			"images/bg3.jpeg"];
+      "images/bg4.gif"];
 
  function changeBackground() {
         $('#entete').css('background-image', 'url('+backgrounds[Math.floor(Math.random()*backgrounds.length)]+')');
@@ -22,20 +20,65 @@ $(document).ready(function () {
   });
 });
 
-$(document).ready(function () {
 var portfolioImage = [  
-    "images/portfolio/final_pad.gif" 
-  ,  "images/portfolio/Lockhart_hogwarts_v1.gif" 
-  ,  "images/portfolio/PA_elixir1.jpg"
-  ,  "images/portfolio/equipe_elixir1.jpg" 
-  ,  "images/portfolio/zoomcours_elixir1.jpg"
-  ,  "images/portfolio/theEndBegins1.gif"
-  ,  "images/portfolio/abistiel9.png"
-  ,  "images/portfolio/NEW_BANN_MM_ENCOURS5.gif"
-  ];
-
-  for(var i=0; i< portfolioImage.length; i++){
-    $( "#conteneurProjet" ).append( $('<div> <ul class="overItemPortfolio"> <li> <a class="www" href="">WWW</a></li> <li> <a class="zoom" href="">ZOOM</a></li><li> <p class="descriptionProjet">Description de l\'image</p></li> </ul> <img src="'+ portfolioImage[i] +'"/> </div>') );
+  { thumbnail: "images/portfolio/final_pad.gif", 
+    href: "http://mimbulusmimbletonia.forumetoile.com/", 
+    zoom: "", 
+    desc: "Bannière d'entête du forum Mimbulus Mimbletonia.  Suite de 14 bannières <br>2016" 
+  }, 
+  { thumbnail: "images/portfolio/Lockhart_hogwarts_v1.gif", 
+    zoom: "", 
+    desc: "Journal intéractif et animé dans le cadre du journal Lockharts & Hogwarts sur MM <br><br>2016"  
+  }, 
+  { thumbnail: "images/portfolio/PA_elixir1.jpg", 
+    zoom: "", 
+    desc: "Page d'accueil du projet scolaire sur la refonte du site du TIM maisonneuve. <br><br>2016"  
+  },
+  { thumbnail: "images/portfolio/equipe_elixir1.jpg", 
+    zoom: "", 
+    desc: "Page description de l'équipe du projet scolaire sur la refonte du site du TIM maisonneuve. <br><br>2016"  
+  }, 
+  { thumbnail: "images/portfolio/zoomcours_elixir1.jpg", 
+    zoom: "", 
+    desc: "Page description d'un cours sélectionné du projet scolaire sur la refonte du site du TIM maisonneuve. <br><br>2016"  
+  },
+  { thumbnail: "images/portfolio/theEndBegins1.gif", 
+    zoom: "", 
+    desc: "Bannière d'entête du forum The End Begins.  Suite de 4 bannières <br><br>2016"
+  },
+  { thumbnail: "images/portfolio/abistiel9.png", 
+    href: "http://theabistielshow.goodforum.net/", 
+    zoom: "", 
+    desc: "Bannière d'entête du forum The Abistiel Show.  Suite de 8 bannières <br><br>2016"  
+  },
+  { thumbnail: "images/portfolio/NEW_BANN_MM_ENCOURS5.gif", 
+    zoom: "", 
+    desc: "Test d'une bannière d'entête du forum Mimbulus Mimbletonia. <br><br>2015"
   }
+];
+
+$(document).ready(function () {
+  for(var i=0; i< portfolioImage.length; i++){
+      console.log(portfolioImage.href)
+    if(portfolioImage[i].href == undefined){    
+        $( "#conteneurProjet" ).append( $('<div> <ul class="overItemPortfolio"> <li> <a class="zoom" href="">ZOOM</a></li><li> <p class="descriptionProjet">'+ portfolioImage[i].desc +'</p></li> </ul> <img src="'+ portfolioImage[i].thumbnail +'"/> </div>') );
+    } else {
+        $( "#conteneurProjet" ).append( $('<div> <ul class="overItemPortfolio"> <li> <a class="www" href="'+ portfolioImage[i].href +'">WWW</a><a class="zoom" href="">ZOOM</a></li><li> <p class="descriptionProjet">'+ portfolioImage[i].desc +'</p></li> </ul> <img src="'+ portfolioImage[i].thumbnail +'"/> </div>') );
+    }
+  }
+
+  $(".menu-opener").click(function(){
+    $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
+  });
+
+  $(".menu-link").click(function(){
+    $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
+  });
+
 });
+
+
+
+
+
   
