c4da68f0058a7840a9186bd7a1f4d32f7cae82bf
Kathleen Blackburn <Kathleen Blackburn>
2017-03-18 22:12:00 -0400

    fonctionnaliter scroll et generation portfolio fonctionnel

--- js/skill.js
+++ js/skill.js
@@ -1,3 +1,4 @@
+/* fonction background aléatoire*/
 (function() {
         var backgrounds = [
 			"images/bg1.jpeg",
@@ -20,3 +21,21 @@ $(document).ready(function () {
     }, 100*i);
   });
 });
+
+$(document).ready(function () {
+var portfolioImage = [  
+    "images/portfolio/final_pad.gif" 
+  ,  "images/portfolio/Lockhart_hogwarts_v1.gif" 
+  ,  "images/portfolio/PA_elixir1.jpg"
+  ,  "images/portfolio/equipe_elixir1.jpg" 
+  ,  "images/portfolio/zoomcours_elixir1.jpg"
+  ,  "images/portfolio/theEndBegins1.gif"
+  ,  "images/portfolio/abistiel9.png"
+  ,  "images/portfolio/NEW_BANN_MM_ENCOURS5.gif"
+  ];
+
+  for(var i=0; i< portfolioImage.length; i++){
+    $( "#conteneurProjet" ).append( $('<div> <ul class="overItemPortfolio"> <li> <a class="www" href="">WWW</a></li> <li> <a class="zoom" href="">ZOOM</a></li><li> <p class="descriptionProjet">Description de l\'image</p></li> </ul> <img src="'+ portfolioImage[i] +'"/> </div>') );
+  }
+});
+  
